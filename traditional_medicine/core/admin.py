3164fb15be8cd4558071a5777302961bd88e6b21
grythman <btuguldur397@gmail.com>
2024-10-07 03:43:33 +0800

    1

--- traditional_medicine/core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Patient, Doctor, Appointment, Payment, MedicalHistory
-
-admin.site.register(Patient)
-admin.site.register(Doctor)
-admin.site.register(Appointment)
-admin.site.register(Payment)
-admin.site.register(MedicalHistory)
--- traditional_medicine/core/tasks.py
+++
@@ -1,8 +0,0 @@
-# tasks.py
-from celery import shared_task
-from .models import Appointment
-
-@shared_task
-def send_reminder_email(appointment_id):
-    appointment = Appointment.objects.get(id=appointment_id)
-    # Send reminder email logic here
